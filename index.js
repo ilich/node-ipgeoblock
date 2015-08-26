eeb1df68056b249463cead48dbce2a6dbeb27863
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-08-26 16:07:35 -0500

    Created filter middleware

---
+++ index.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var fs = require("fs");
+var requestIp = require("request-ip");
+var MMDBReader = require("mmdb-reader");
+
+module.exports = function (options) {
+	
+	options = options || {};
+	verifyOptions(options);
+	
+	var mmdb = new MMDBReader(options.geolite2);
+	
+	function verifyOptions() {
+		if (!options.geolite2) {
+			throw new Error("options.geolite2 is not set");
+		}
+		
+		fs.accessSync(options.geolite2, fs.R_OK);
+		
+		options.blocked = options.blocked || [];
+		options.blockedCountries = options.blockedCountries || [];
+		options.allowedCountries = options.allowedCountries || [];
+		
+		if (options.blockedCountries.length > 0 && options.allowedCountries.length > 0) {
+			throw new Error("You have to choose only allowed contries or only blocked countries");
+		}
+	}
+	
+	function getIP(req) {
+		var ip = requestIp.getClientIp(req);
+		if (ip !== null) {
+			ip = ip.split(":");
+			ip = ip[ip.length - 1];
+		}
+		
+		return ip;
+	}
+	
+	function isBlocked(ip, res) {
+		// 1. Check that IP address is blocked
+		if (options.blocked.indexOf(ip) > -1) {
+			return true;
+		}
+		
+		var query = mmdb.lookup(ip);
+		if (options.blockedCountries.length > 0) {
+			
+			// 2. If user added country to Blocked Countries collection then only those countries 
+            // are blocked 
+				
+			return query !== null && options.blockedCountries.indexOf(query.country.iso_code) > -1;	
+		} else if (options.allowedCountries.length > 0) {
+			
+			// 3. If user added country to Allowed Countries collecction then all countries except allowed
+            // are blocked
+				
+			return query === null || options.allowedCountries.indexOf(query.country.iso_code) === -1;
+		}
+		
+		return false;
+	}
+	
+	return function (req, res, next) {
+		var ip = getIP(req);
+		if (isBlocked(ip, res)) {
+			res.statusCode = 403;
+			res.end("Forbidden");
+			return;	
+		}
+		
+		next();
+	};
+	
+};
---
+++ test/index.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var ipgeoblock = require("..");
+var assert = require("assert");
+var connect = require("connect");
+var request = require("supertest");
+
+describe("node-ipgeoblock", function () {
+	
+	function createApp(isWhitelist) {
+		var blacklist = isWhitelist ? [] : ["FR"];
+		var whitelist = isWhitelist ? ["FR"] : [];
+		
+		var app = connect();
+		
+		app.use(ipgeoblock({
+			geolite2: "./test/GeoLite2-Country.mmdb",
+			blocked: ["192.168.0.1", "192.168.0.3", "192.168.0.4"],
+			blockedCountries: blacklist,
+			allowedCountries: whitelist
+		}));
+		
+		app.use(function (req, res) {
+			res.end("OK");
+		});	
+		
+		return app;
+	}
+	
+	var app = createApp(false);
+	
+	it("block specific IP address", function (done) {
+		request(app).get("/")
+			.set("x-client-ip", "192.168.0.1")
+			.expect(403)
+			.expect("Forbidden", done);
+	});
+	
+	it("allow IP address when IP blacklist is used", function (done) {
+		request(app).get("/")
+			.set("x-client-ip", "192.168.0.2")
+			.expect(200)
+			.expect("OK", done);
+	});
+	
+	it("block France IP address when only France is in blackist", function (done) {
+		request(app).get("/")
+			.set("x-client-ip", "213.128.63.255")
+			.expect(403)
+			.expect("Forbidden", done);
+	});
+	
+	it("allow Greece IP address when ony only France is in blacklist", function (done) {
+		request(app).get("/")
+			.set("x-client-ip", "46.246.128.0")
+			.expect(200)
+			.expect("OK", done);
+	});
+	
+	it("allow unknown IP address when ony only France is in blacklist", function (done) {
+		request(app).get("/")
+			.set("x-client-ip", "192.168.0.2")
+			.expect(200)
+			.expect("OK", done);
+	});
+	
+	it("allow France IP address when only France is in whitelist", function (done) {
+		var appWhitelist = createApp(true);
+		
+		request(appWhitelist).get("/")
+			.set("x-client-ip", "213.128.63.255")
+			.expect(200)
+			.expect("OK", done);
+	});
+	
+	it("block Greece IP address when ony only France is in whitelist", function (done) {
+		var appWhitelist = createApp(true);
+		
+		request(appWhitelist).get("/")
+			.set("x-client-ip", "46.246.128.0")
+			.expect(403)
+			.expect("Forbidden", done);
+	});
+	
+	
+	it("block unknows IP address when ony only France is in whitelist", function (done) {
+		var appWhitelist = createApp(true);
+		
+		request(appWhitelist).get("/")
+			.set("x-client-ip", "192.168.0.2")
+			.expect(403)
+			.expect("Forbidden", done);
+	});
+});
